2a72233e4ebf0572538e6b6345be3625399fbed8
Justine Hell <hell.justine@gmail.com>
2023-05-11 13:44:41 +0200

    WIP

--- templates/terraform_test_application/app.tf
+++ templates/terraform_test_application/app.tf
@@ -1,6 +1,6 @@
-{% set securityGroupId = generateId('SecurityGroup_') %}
-{% set vpcId = generateId('VPC_') %}
-{% set internetGatewayId = generateId('InternetGateway_') %}
+{% set securityGroupId = generateId('SecurityGroup', 'SecurityGroup_') %}
+{% set vpcId = generateId('VPC', 'VPC_') %}
+{% set internetGatewayId = generateId('InternetGateway', 'InternetGateway_') %}
 resource "aws_security_group" "{{securityGroupId}}" {
     vpc_id = ["{{vpcId}}"]
 }
